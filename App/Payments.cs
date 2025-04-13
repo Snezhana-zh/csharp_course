66ff5779df6fac44db516a451a014ce3bec80abb
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-04-14 01:45:48 +0700

    done task5

--- App/Payments.cs
+++ App/Payments.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace App;
 
 public enum PaymentsPlan
@@ -10,7 +12,38 @@ public class Payments
 {
     public static decimal CalculateTotalPayments(PaymentsPlan plan, decimal rate, int monthsCount, decimal amount)
     {
-        //TODO напишите тут свое решение
-        throw new NotImplementedException();
+        if (monthsCount <= 0 || amount <= 0 || rate < 0)
+            throw new ArgumentException("Invalid parameters");
+        
+        return plan switch
+        {
+            PaymentsPlan.Differentiated => CalculateDifferentiated(amount, rate / 12 / 100, monthsCount),
+            PaymentsPlan.Annuity => CalculateAnnuity(amount, rate / 12 / 100, monthsCount),
+            _ => throw new ArgumentException("Unknown payment plan")
+        };
+    }
+    private static decimal CalculateDifferentiated(decimal amount, decimal rate, int monthsCount)
+    {
+        decimal total = 0;
+        decimal mainDebt = amount / monthsCount;
+        for (int month = 1; month <= monthsCount; month++)
+        {
+            decimal remainingDebt = amount - (mainDebt * (month - 1));
+            decimal percentages = remainingDebt * rate ;
+            total += mainDebt + percentages;
+        }
+        return Math.Round(total, 2);
+    }
+
+    private static decimal CalculateAnnuity(decimal amount, decimal rate, int monthsCount)
+    {
+        if (rate == 0)
+            return amount;
+            
+        decimal annuityCoefficient = rate * (decimal)Math.Pow(1 + (double)rate, monthsCount) / 
+                                     ((decimal)Math.Pow(1 + (double)rate, monthsCount) - 1);
+        
+        decimal monthlyPayment = amount * annuityCoefficient;
+        return Math.Round(monthlyPayment * monthsCount, 2);
     }
 }
--- AppTests/PaymentsTests.cs
+++ AppTests/PaymentsTests.cs
@@ -4,12 +4,13 @@ namespace AppTests;
 
 public class PaymentsTests
 {
-    //TODO напишите свои тесты
-    [TestCase(PaymentsPlan.Annuity, 7, 3, 10000, 10116.90)]
+    [TestCase(PaymentsPlan.Annuity, 7, 3, 10000, 10116.89)]
     [TestCase(PaymentsPlan.Differentiated, 3, 5, 200000, 201500)]
+    [TestCase(PaymentsPlan.Annuity, 25, 36, 5000000, 7156768.68)]
+    [TestCase(PaymentsPlan.Differentiated, 25, 36, 5000000, 6927083.33)]
     public void TestPasses_When_Result_Correct(PaymentsPlan plan, decimal rate, int monthsCount, decimal amount, decimal expected)
     {
         var actual = Payments.CalculateTotalPayments(plan, rate, monthsCount, amount);
-        Assert.That(actual, Is.EqualTo(expected));
+        Assert.That(actual, Is.EqualTo(expected).Within(1e-1));
     }
 }
