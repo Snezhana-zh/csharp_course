463c39dd98cd1099a58714f0d4740b5d965ebee3
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-04-13 13:30:09 +0700

    done task1

--- AppTests/PricesTests.cs
+++ AppTests/PricesTests.cs
@@ -4,9 +4,26 @@ namespace AppTests;
 
 public class PricesTests
 {
-    //TODO напишите свои тесты
-    [TestCase(1000052, "рубля")]
+    [TestCase(1, "рубль")]
+    [TestCase(2, "рубля")]
+    [TestCase(5, "рублей")]
+    
+    [TestCase(0, "рублей")]
+    [TestCase(10, "рублей")]
+    [TestCase(11, "рублей")]
+    [TestCase(12, "рублей")]
+    [TestCase(20, "рублей")]
+    [TestCase(21, "рубль")]
+    [TestCase(22, "рубля")]
     [TestCase(25, "рублей")]
+    
+    [TestCase(100, "рублей")]
+    [TestCase(101, "рубль")]
+    [TestCase(102, "рубля")]
+    [TestCase(105, "рублей")]
+    [TestCase(111, "рублей")]
+    [TestCase(121, "рубль")]
+    [TestCase(1000052, "рубля")]
     public void TestPasses_When_Result_Correct(int price, string expected)
     {
         var actual = Prices.GetCurrencyAlias(price, false, false);
