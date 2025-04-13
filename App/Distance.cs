ed5cfb3c5ff627c83131caad6f842f8c75752bb4
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-04-14 00:50:48 +0700

    done task4

--- App/Distance.cs
+++ App/Distance.cs
@@ -8,7 +8,25 @@ public static class Distance
         // отрезок
         double x1, double y1, double x2, double y2)
     {
-        //TODO напишите тут свое решение
-        throw new NotImplementedException();
+        double A = y1 - y2;
+        double B = x2 - x1;
+        double C = x1 * y2 - x2 * y1;
+
+        double D = A * y - B * x;
+        if (Math.Abs(B) < 1e-6 && Math.Abs(A) < 1e-6)
+        {
+            return Math.Sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y));
+        }
+        double x_ = (-1 * C * A - B * D) / (A * A + B * B);
+        double y_ = (D * A - B * C) / (A * A + B * B);
+
+        if (x_ > x2 || x_ < x1 || ((y_ > y2 || y_ < y1) && y1 < y2) || ((y_ < y2 || y_ > y1) && y1 >= y2))
+        {
+            double distanceFirstPoint = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
+            double distanceSecondPoint = Math.Sqrt((x - x2) * (x - x2) + (y - y2) * (y - y2));
+            if (distanceFirstPoint > distanceSecondPoint) return distanceSecondPoint;
+            else return distanceFirstPoint;
+        }
+        return Math.Abs(A * x + B * y + C) / Math.Sqrt(B * B + A * A);
     }
 }
