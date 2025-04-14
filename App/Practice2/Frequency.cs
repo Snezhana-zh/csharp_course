c66040e5f8ad60a5e88129d584f0f32f3d5d7120
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-04-14 18:39:41 +0700

    done task4

---
+++ App/Practice2/Frequency.cs
@@ -0,0 +1,70 @@
+﻿namespace App.Practice2;
+
+public static class Frequency
+{
+    public static Dictionary<string, string> FrequencyAnalysis(string inputString) 
+    {
+        var dictionary = new Dictionary<string, string>();
+        var sentences = inputString.Split('.', StringSplitOptions.RemoveEmptyEntries);
+        
+        var stats = new Dictionary<string, Dictionary<string, int>>();
+        
+        foreach (var sentence in sentences)
+        {
+            var words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                var current = words[i];
+                var next = words[i + 1];
+            
+                if (!stats.ContainsKey(current))
+                    stats[current] = new Dictionary<string, int>();
+            
+                if (!stats[current].ContainsKey(next))
+                    stats[current][next] = 0;
+                
+                stats[current][next]++;
+            }
+            for (int i = 0; i < words.Length - 2; i++)
+            {
+                var current = words[i] + ' ' + words[i + 1];
+                var next = words[i + 2];
+            
+                if (!stats.ContainsKey(current))
+                    stats[current] = new Dictionary<string, int>();
+            
+                if (!stats[current].ContainsKey(next))
+                    stats[current][next] = 0;
+                
+                stats[current][next]++;
+            }
+        }
+
+        FindMostFreq(stats, dictionary);
+        return dictionary;
+    }
+
+    private static void FindMostFreq(Dictionary<string, Dictionary<string, int>> stats,
+        Dictionary<string, string> dictionary)
+    {
+        foreach (var pair in stats)
+        {
+            int max = 0;
+            string maxStr = null;
+            foreach (var elem in pair.Value)
+            {
+                if (elem.Value > max)
+                {
+                    maxStr = elem.Key;
+                    max = elem.Value;
+                }
+                else if (elem.Value == max && String.CompareOrdinal(maxStr, elem.Key) > 0)
+                {
+                    maxStr = elem.Key;
+                }
+            }
+            dictionary[pair.Key] = maxStr;
+        }
+    }
+}
---
+++ AppTests/Practice2/FrequencyTests.cs
@@ -0,0 +1,49 @@
+﻿using App.Practice2;
+
+namespace AppTests.Practice2;
+
+public class FrequencyTests
+{
+    [Test]
+    public void TestPasses_When_Result_Correct()
+    {
+        string input = "a b c d. b c d. e b c a d.";
+        var expectedResult = new Dictionary<string, string>
+        {
+            { "a", "b" },
+            { "b", "c" },
+            { "c", "d" },
+            { "e", "b" },
+            { "a b", "c" },
+            { "b c", "d" },
+            { "e b", "c" },
+            { "c a", "d" }
+        };
+        var result = Frequency.FrequencyAnalysis(input);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+    [Test]
+    public void TestPasses_When_Result_Correct_Empty()
+    {
+        string input = "a.";
+        var expectedResult = new Dictionary<string, string>();
+        var result = Frequency.FrequencyAnalysis(input);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+    [Test]
+    public void TestPasses_When_Result_Correct_Words()
+    {
+        string input = "She stood up. Then she left.";
+        var expectedResult = new Dictionary<string, string>
+        {
+            { "She", "stood" },
+            { "stood", "up" },
+            { "Then", "she" },
+            { "she", "left" },
+            { "She stood", "up" },
+            { "Then she", "left" }
+        };
+        var result = Frequency.FrequencyAnalysis(input);
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
