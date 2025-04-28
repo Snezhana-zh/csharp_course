511d177b2699b09fd680bfffa36236af8f5af7c7
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-04-28 21:30:37 +0700

    added methods get distance and get projection

--- App/Distance.cs
+++ App/Distance.cs
@@ -8,25 +8,42 @@ public static class Distance
         // отрезок
         double x1, double y1, double x2, double y2)
     {
-        double A = y1 - y2;
-        double B = x2 - x1;
-        double C = x1 * y2 - x2 * y1;
+        var tolerance = 1e-6;
+        var A = y1 - y2;
+        var B = x2 - x1;
+        var C = x1 * y2 - x2 * y1;
 
-        double D = A * y - B * x;
-        if (Math.Abs(B) < 1e-6 && Math.Abs(A) < 1e-6)
+        var D = A * y - B * x;
+        if (Math.Abs(B) < tolerance && Math.Abs(A) < tolerance)
         {
-            return Math.Sqrt((x2 - x) * (x2 - x) + (y2 - y) * (y2 - y));
+            return GetDistance(x2, y2, x, y);
         }
-        double x_ = (-1 * C * A - B * D) / (A * A + B * B);
-        double y_ = (D * A - B * C) / (A * A + B * B);
 
-        if (x_ > x2 || x_ < x1 || ((y_ > y2 || y_ < y1) && y1 < y2) || ((y_ < y2 || y_ > y1) && y1 >= y2))
+        if (PointInSegment(x1, y1, x2, y2, GetProjection(A, B, C, D).Item1, GetProjection(A, B, C, D).Item2))
         {
-            double distanceFirstPoint = Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1));
-            double distanceSecondPoint = Math.Sqrt((x - x2) * (x - x2) + (y - y2) * (y - y2));
-            if (distanceFirstPoint > distanceSecondPoint) return distanceSecondPoint;
-            else return distanceFirstPoint;
+            var distanceFirstPoint = GetDistance(x, y, x1, y1);
+            var distanceSecondPoint = GetDistance(x, y, x2, y2);
+            return distanceFirstPoint > distanceSecondPoint ? distanceSecondPoint : distanceFirstPoint;
         }
         return Math.Abs(A * x + B * y + C) / Math.Sqrt(B * B + A * A);
+    }
+    private static double GetDistance(double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+    }
+    private static Tuple<double, double> GetProjection(double A, double B, double C, double D)
+    {
+        var x = (-1 * C * A - B * D) / (A * A + B * B);
+        var y = (D * A - B * C) / (A * A + B * B);
+        return new Tuple<double, double>(x, y);
+    }
+    private static bool PointInSegment(double x1, double y1, double x2, double y2, double x3, double y3)
+    {
+        return x3 > x2
+                || x3 < x1
+                || ((y3 > y2 || y3 < y1)
+                    && y1 < y2)
+                || ((y3 < y2 || y3 > y1)
+                    && y1 >= y2);
     }
 }
