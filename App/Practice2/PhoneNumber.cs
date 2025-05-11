4e12c8d910f1f8e7cbdfe9956701d2bf9ace2982
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-05-12 00:47:21 +0700

    add static field for regex constant

--- App/Practice2/PhoneNumber.cs
+++ App/Practice2/PhoneNumber.cs
@@ -4,15 +4,15 @@ namespace App.Practice2;
 
 public static class PhoneNumber
 {
+    private static readonly Regex PhoneRegex =
+        new Regex(@"(8|\+7|7)+[\s-\(]*(9\d{2})[\s-\)]*\d{3}[\s-]*\d{2}[\s-]*\d{2}", RegexOptions.Compiled);
     public static bool TryParsePhone(string inputString, out string parsedPhone) 
     {
         parsedPhone = null;
-        var phonePattern = @"(?<!\+)(8|\+7|7)+[\s-\(]*(9\d{2})[\s-\)]*\d{3}[\s-]*\d{2}[\s-]*\d{2}";
-        var match = Regex.Match(inputString, phonePattern);
+        var match = PhoneRegex.Match(inputString);
         if (!match.Success) return false;
         
         var digits = new String(match.Value.Where(char.IsDigit).ToArray());
-        Console.WriteLine(digits);
 
         if (digits.Length == 11)
         {
--- AppTests/Practice2/PhoneNumberTests.cs
+++ AppTests/Practice2/PhoneNumberTests.cs
@@ -10,12 +10,12 @@ public class PhoneNumberTests
     [TestCase("Мой номер +7 983 313-68-27, а код 4567", true, "+79833136827")]
     [TestCase("8983-313-68-27", true, "+79833136827")]
     [TestCase("Мой номер ---983 313-68-27, а код 4567", false, null)]
-    [TestCase("Мой номер +8983 313-68-27, а код 4567", false, null)]
+    [TestCase("Мой номер +8983 313-68-27, а код 4567", true, "+79833136827")]
     [TestCase("Мой номер +983 313-68-27, а код 4567", false, null)]
     [TestCase("Мой номер +9 983 313-68-27, а код 4567", false, null)]
     [TestCase("Мой номер 79833136827, а код 4567", true, "+79833136827")]
     [TestCase("Мой номер +10-983-313-68-27, а код 4567", false, null)]
-    public void TestPasses_When_Result_Correct(string msg, bool expectedRes, string expectedPhone)
+    public void TestPasses_When_Phone_Successfully_Parsed(string msg, bool expectedRes, string expectedPhone)
     {
         var actual = PhoneNumber.TryParsePhone(msg, out var res);
         Assert.That(actual, Is.EqualTo(expectedRes));
