0e085fc5bb738ec7b2876b5678c96639b20a2586
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-05-13 00:12:00 +0700

    done task1

---
+++ App/Practice4/I2DVertex.cs
@@ -0,0 +1,7 @@
+﻿namespace App.Practice4;
+
+public interface I2DVertex
+{
+    double X { get; set; }
+    double Y { get; set; }
+}
---
+++ App/Practice4/Square.cs
@@ -0,0 +1,13 @@
+﻿namespace App.Practice4;
+
+public sealed class Square : Rectangle
+{
+    public Square(Vertex a, Vertex b, Vertex c, Vertex d) : base(a, b, c, d)
+    {
+        if (!IsValidPolygon()) throw new ArgumentException("Invalid Square");
+    }
+    protected override bool IsValidPolygon()
+    {
+        return Math.Abs(GetDistance(Vertices[0], Vertices[1]) - GetDistance(Vertices[1], Vertices[2])) < Tolerance;
+    }
+}
