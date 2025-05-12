09f9ee970493756bd3e9bbc54a9ff386c0f1a9a0
Snezhana Zhuiko <zhuikosnezhana@gmail.com>
2025-05-12 16:29:14 +0700

    added tests

--- App/Practice3/User.cs
+++ App/Practice3/User.cs
@@ -3,35 +3,37 @@ using System.Text.RegularExpressions;
 
 public class User
 {
-    private readonly Guid id;
-    private readonly string login;
-    private string password;
-    private string name;
-    private string surname;
-    private readonly string inn;
-    private string phone;
-    private DateTime registerDate;
+    private readonly Guid _id;
+    private readonly string _login;
+    private string _password;
+    private string _name;
+    private string _surname;
+    private readonly string _inn;
+    private string _phone;
+    private readonly DateTime _registerDate;
+    
+    private static readonly Regex PhoneRegex =
+        new Regex(@"(8|\+7|7)+[\s-\(]*(9\d{2})[\s-\)]*\d{3}[\s-]*\d{2}[\s-]*\d{2}", RegexOptions.Compiled);
 
     public User(string login, string password, string name, string surname, string inn, string phone)
     {
-        registerDate = DateTime.Now;
-        this.login = login;
-        this.password = password;
-        this.name = name;
-        this.surname = surname;
-        this.inn = inn;
-        _ =  TryParsePhone(phone, out this.phone);
-        id = Guid.NewGuid();
+        _registerDate = DateTime.Now;
+        _login = login;
+        _password = password;
+        _name = name;
+        _surname = surname;
+        _inn = inn;
+        _phone = phone;
+        _id = Guid.NewGuid();
     }
     public string GetUserFullName()
     {
-        return name + " " + surname;
+        return _name + " " + _surname;
     }
-    private static bool TryParsePhone(string inputString, out string parsedPhone)
+    private static bool TryParsePhone(string inputString, out string parsedPhone) 
     {
         parsedPhone = null;
-        var phonePattern = @"(?<!\+)(8|\+7|7)+[\s-\(]*(9\d{2})[\s-\)]*\d{3}[\s-]*\d{2}[\s-]*\d{2}";
-        var match = Regex.Match(inputString, phonePattern);
+        var match = PhoneRegex.Match(inputString);
         if (!match.Success) return false;
         
         var digits = new String(match.Value.Where(char.IsDigit).ToArray());
@@ -44,12 +46,11 @@ public class User
         }
         return false;
     }
-
     public bool TryUpdatePhone(string phone)
     {
         var isValidPhone =  TryParsePhone(phone, out string validPhone);
         if (!isValidPhone) return false;
-        this.phone = validPhone;
+        _phone = validPhone;
         return true;
     }
 }
---
+++ AppTests/Practice3/UserTests.cs
@@ -0,0 +1,49 @@
+﻿using App.Practice3;
+namespace AppTests.Practice3;
+
+public class UserTests
+{
+    [Test]
+    public void TestPasses_When_Get_Correct_Full_Name()
+    {
+        User user = new User(
+            "new_user",
+            "mySecurePassword123",
+            "Мария",
+            "Иванова",
+            "998877665544",
+            "+7 (912) 345-67-89");
+        
+        var actual = user.GetUserFullName();
+        var expectedRes = "Мария " + "Иванова";
+        Assert.That(actual, Is.EqualTo(expectedRes));
+    }
+    [Test]
+    public void TestPasses_When_Successfully_Update_Phone()
+    {
+        User user = new User(
+            "new_user",
+            "mySecurePassword123",
+            "Мария",
+            "Иванова",
+            "998877665544",
+            "+7 (912) 345-67-89");
+
+            var actual = user.TryUpdatePhone("89161234567");
+            Assert.That(actual, Is.True);
+    }
+    [Test]
+    public void TestPasses_When_Not_Update_Phone()
+    {
+        User user = new User(
+            "new_user",
+            "mySecurePassword123",
+            "Мария",
+            "Иванова",
+            "998877665544",
+            "+7 (912) 345-67-89");
+
+        var actual = user.TryUpdatePhone("8916");
+        Assert.That(actual, Is.False);
+    }
+}
